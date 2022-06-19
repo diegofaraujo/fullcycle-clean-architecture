6c103fde261c66eee621dfc138e7530f28680b3b
Diego Fernando de Araujo <diego.araujo@pharmalog.io>
2022-06-19 00:17:02 -0300

    Notification pattern em Products

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -1,22 +1,45 @@
 import Product from "./product";
+import ProductB from "./product-b";
 
 describe("Product unit tests", () => {
   it("should throw error when id is empty", () => {
     expect(() => {
-      const product = new Product("", "Product 1", 100);
-    }).toThrowError("Id is required");
+      new Product("", "Product 1", 100);
+    }).toThrowError("product: Id is required");
   });
 
   it("should throw error when name is empty", () => {
     expect(() => {
-      const product = new Product("123", "", 100);
-    }).toThrowError("Name is required");
+      new Product("123", "", 100);
+    }).toThrowError("product: Name is required");
   });
 
   it("should throw error when price is less than zero", () => {
     expect(() => {
-      const product = new Product("123", "Name", -1);
-    }).toThrowError("Price must be greater than zero");
+      new Product("123", "Name", -1);
+    }).toThrowError("product: Price must be greater than zero");
+  });
+
+  it("should throw error when id and name is empty", () => {
+    expect(() => {
+      new Product("", "", 10);
+    }).toThrowError("product: Id is required,product: Name is required");
+  });
+
+  it("should throw error when name is empty and price is less than zero", () => {
+    expect(() => {
+      new Product("123", "", -0.1);
+    }).toThrowError(
+      "product: Name is required,product: Price must be greater than zero"
+    );
+  });
+
+  it("should throw error when id and name is empty and price is less than zero", () => {
+    expect(() => {
+      new ProductB("", "", -0.1);
+    }).toThrowError(
+      "product: Id is required,product: Name is required,product: Price must be greater than zero"
+    );
   });
 
   it("should change name", () => {
--- src/usecase/product/update/update.product.unit.spec.ts
+++ src/usecase/product/update/update.product.unit.spec.ts
@@ -1,37 +1,39 @@
+import ProductInterface from "../../../domain/product/entity/product.interface";
 import ProductFactory from "../../../domain/product/factory/product.factory";
 import UpdateProductUseCase from "./update.product.usecase";
 
-const mockProdut = ProductFactory.create("a", "Product", 10);
-
-const input = {
-  id: mockProdut.id,
-  name: "Product ABC Updated",
-  price: 12.5,
-};
-
-const MockRepository = () => {
-  return {
-    find: jest.fn().mockResolvedValue(mockProdut),
-    findAll: jest.fn(),
-    update: jest.fn(),
-    create: jest.fn(),
-  };
-};
-
 describe("Test update product use case", () => {
-  it("should update a product", async () => {
-    const productRepository = MockRepository();
-    const productUpdateUseCase = new UpdateProductUseCase(productRepository);
+  let productUpdateUseCase: UpdateProductUseCase;
+  let mockProdut: ProductInterface;
 
+  beforeEach(() => {
+    mockProdut = ProductFactory.create("a", "Product", 10);
+
+    const MockRepository = () => {
+      return {
+        find: jest.fn().mockResolvedValue(mockProdut),
+        findAll: jest.fn(),
+        update: jest.fn(),
+        create: jest.fn(),
+      };
+    };
+
+    const productRepository = MockRepository();
+    productUpdateUseCase = new UpdateProductUseCase(productRepository);
+  });
+
+  it("should update a product", async () => {
+    const input = {
+      id: mockProdut.id,
+      name: "Product ABC Updated",
+      price: 12.5,
+    };
     const output = await productUpdateUseCase.execute(input);
 
     expect(output).toEqual(input);
   });
 
   it("should throw an error when product name is empty", async () => {
-    const productRepository = MockRepository();
-    const productUpdateUseCase = new UpdateProductUseCase(productRepository);
-
     const input = {
       id: mockProdut.id,
       name: "",
@@ -44,9 +46,6 @@ describe("Test update product use case", () => {
   });
 
   it("should throw an error when product price is lower than zero", async () => {
-    const productRepository = MockRepository();
-    const productUpdateUseCase = new UpdateProductUseCase(productRepository);
-
     const input = {
       id: mockProdut.id,
       name: "Product",
